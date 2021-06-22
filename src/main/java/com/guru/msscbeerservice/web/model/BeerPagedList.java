39139e0069a7bd1dba532ed28ffb22f716883496
shuhail-allianceit <42164094+shuhail-allianceit@users.noreply.github.com>
2021-06-22 13:20:50 +0530

    added web model

---
+++ src/main/java/com/guru/msscbeerservice/web/model/BeerPagedList.java
@@ -0,0 +1,13 @@
+package com.guru.msscbeerservice.web.model;
+
+
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public class BeerPagedList extends PageImpl<BeerDto> {
+    public BeerPagedList(List<BeerDto> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+}
