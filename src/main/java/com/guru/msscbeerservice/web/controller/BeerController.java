8ff8aa0f4114f52ccf83c0bd2034cef6a2a7f005
ahamedshaik16 <ahamedshaik16@gmail.com>
2021-07-07 15:14:11 +0530

    Added service

--- src/main/java/com/guru/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/com/guru/msscbeerservice/web/controller/BeerController.java
@@ -1,6 +1,8 @@
 package com.guru.msscbeerservice.web.controller;
 
+import com.guru.msscbeerservice.services.BeerService;
 import com.guru.msscbeerservice.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -8,24 +10,29 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/beer")
 @RestController
 public class BeerController {
 
+    private final BeerService beerService;
+
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
-
-        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+        return new ResponseEntity<>(beerService.getById(beerId), HttpStatus.OK);
+//        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity saveNewBeer(@Validated @RequestBody BeerDto beerDto) {
-        return  new ResponseEntity(HttpStatus.CREATED);
+//        return  new ResponseEntity(HttpStatus.CREATED);
+        return new ResponseEntity<>(beerService.saveNewBeer(beerDto), HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
     public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId,
                                          @Validated @RequestBody BeerDto beerDto) {
-        return new ResponseEntity(HttpStatus.NO_CONTENT);
+//        return new ResponseEntity(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(beerService.updateBeer(beerId, beerDto), HttpStatus.NO_CONTENT);
     }
 }
---
+++ src/main/java/com/guru/msscbeerservice/web/controller/NotFoundException.java
@@ -0,0 +1,4 @@
+package com.guru.msscbeerservice.web.controller;
+
+public class NotFoundException extends RuntimeException{
+}
